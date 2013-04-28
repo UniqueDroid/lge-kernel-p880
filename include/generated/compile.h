37413b74955beee0017ac71c59fa7e2ffe99a307
UniqueDroid <jn@unique-production.com>
2013-04-28 11:48:36 +0200

    upload stock/CM kernel source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Sat Apr 27 22:14:21 CEST 2013"
+#define LINUX_COMPILE_BY "t-800"
+#define LINUX_COMPILE_HOST "SkyNet"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.1.10-uniquedroid-x3-0.0.2"
